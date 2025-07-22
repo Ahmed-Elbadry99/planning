12496ca5521d3f1ab99103ce76fa8fcc2947d142
elbadry <elbadry.egypt010@gmail.com>
2025-07-22 17:10:51 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#membershipAccordion .card-header').on('click', function () {
+      // اغلق الكل
+      $('#membershipAccordion .card-header').attr('aria-expanded', 'false');
+      // لو العنصر مش مفتوح، افتحه
+      var target = $(this).data('target');
+      if (!$(target).hasClass('show')) {
+        $(this).attr('aria-expanded', 'true');
+      }
+    });
+  });
